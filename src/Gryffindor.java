561ebe0d8c50213ee401283370262a1b751075a4
79998 <lekha-fadeev@mail.ru>
2023-06-16 20:28:16 +0300

    коммит

---
+++ src/Gryffindor.java
@@ -0,0 +1,32 @@
+public class Gryffindor extends Hogwarts {
+
+    private int nobility;
+    private int honor;
+    private int courage;
+
+    public Gryffindor(String name, int powerOfMagic, int transgressionDistance, int nobility, int honor, int courage) {
+        super(name, powerOfMagic, transgressionDistance);
+        this.nobility = nobility;
+        this.honor = honor;
+        this.courage = courage;
+    }
+
+    @Override
+    int calculateSpecificScore() {
+        return this.nobility + this.courage + this.honor;
+    }
+
+    @Override
+    void printCompareOfStudents(Hogwarts best, Hogwarts worst) {
+        System.out.println(String.format("%s лучший Гриффиндорец, чем %s", best.getName(),worst.getName()));
+    }
+
+    @Override
+    public String toString() {
+        return "Gryffindor{" +
+                "nobility=" + nobility +
+                ", honor=" + honor +
+                ", courage=" + courage +
+                '}';
+    }
+}
